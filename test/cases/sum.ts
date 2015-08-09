5e2c3639c3d31b261f728a389bdc64d5f78951a0
thr0w <thr0w@users.noreply.github.com>
2015-08-09 11:07:26 -0300

    store initial + actions + catches + listenners (sum)

--- test/cases/sum.ts
+++ test/cases/sum.ts
@@ -1,4 +1,4 @@
-import {createAction, createEvent, Disposable, Event, Reference, createDisposable, DisposableChildren} from "../../lib/h5flux";
+import {createAction, createEvent, Disposable, Event, asap, EventToggle, EventListener, Reference, createDisposable, DisposableChildren} from "../../lib/h5flux";
 
 export var ev_sum_result = createEvent<number>("sum-result")
 export var ac_sum = createAction({
@@ -13,15 +13,24 @@ export var ac_sum = createAction({
 
 interface Store<STATE> extends Reference {
     getState(): STATE;
+    changed: {
+        on: EventToggle<STATE>,
+        off: EventToggle<STATE>,
+    }
 }
 
 export function createStore<STATE, T extends Reference, ACTIONS extends DisposableChildren>(
     initialState: STATE, actions: ACTIONS, catches: Event<STATE>[], createInstance: (state: STATE, action: ACTIONS) => T) {
     return createDisposable(actions, (actions) => {
         var state: STATE = initialState;
+        var listenners: EventListener<STATE>[] = [];
         var instance = createInstance(state, actions);
         type INSTANCE = Store<STATE> & typeof instance;
         (<INSTANCE>instance).getState = () => state;
+        (<INSTANCE>instance).changed = {
+            on: add_listenner,
+            off: remove_listenner,
+        }
         catches.forEach((e) => e.on(changed));
         return {
             instance: (<INSTANCE>instance),
@@ -29,8 +38,18 @@ export function createStore<STATE, T extends Reference, ACTIONS extends Disposab
                 catches.forEach((e) => e.off(changed));
             }
         }
+        function add_listenner(l: EventListener<STATE>) {
+            if (listenners.indexOf(l) == -1)
+                listenners.push(l);
+        }
+        function remove_listenner(l: EventListener<STATE>) {
+            var i = listenners.indexOf(l);
+            if (i >= 0)
+                listenners.splice(i, 1);
+        }
         function changed(newState: STATE) {
             state = newState;
+            listenners.forEach((l) => asap(() => l(newState)))
         }
     });
 }
--- test/h5stores.ts
+++ test/h5stores.ts
@@ -23,7 +23,7 @@ describe('h5-store', () => {
     });
 
 
-    it('sum 0 + 1', (done) => {
+    it('sum 0 + 1 - getState', (done) => {
 
         var sum_store = createSumStore.addRef();
         expect(sum_store.getState(), 'initialState').to.be.equal(0);
@@ -33,40 +33,58 @@ describe('h5-store', () => {
             sum_store.releaseRef();
             done();
         }, 25);
-        //sum_store.
 
-        //
-        // acao.exec=(r){
-        //    return {x}
-        // }
-        // acao.exec(estoriaref);
-        // acao.on()
-
-        // var s = createStore(0, instance);
-        // expect(s.refCount(1), 's.refCount').to.equals(0);
-        // var ref1 = s.addRef(1);
-        // ref1.
-        //
-        // key = 1;
-        // ref1.data().name = "Anne";
-        // ref1.data().age = 20;
-        // expect(s.refCount(1), 's.refCount 1').to.equals(1);
-        // var ref2 = s.addRef(1);
-        // expect(s.refCount(1), 's.refCount 2').to.equals(2);
-        // expect(ref2.data.name, "ref2.data.name").to.equals("Anne");
-        // expect(ref2.data.age, "ref2.data.age").to.equals(20);
-        // ref2.data.age = 21;
-        // expect(ref1.data.age, "ref1.data.name").to.equals(21);
-        // ref1.releaseRef();
-        // expect(s.refCount(1), 's.refCount -1').to.equals(1);
-        // ref1.releaseRef();
-        // expect(s.refCount(1), 's.refCount -1r').to.equals(1);
-        // ref2.releaseRef();
-        // expect(s.refCount(1), 's.refCount -2').to.equals(0);
-        // ref2.releaseRef();
-        // expect(s.refCount(1), 's.refCount -2r').to.equals(0);
-        // done();
     });
+
+    it('sum 0 + 1 - listen', (done) => {
+
+        var sum_store = createSumStore.addRef();
+        expect(sum_store.getState(), 'initialState').to.be.equal(0);
+        sum_store.sum(1);
+        sum_store.changed.on((state) => {
+
+            expect(state, 'one added - arg').to.be.equal(1);
+
+            expect(sum_store.getState(), 'one added').to.be.equal(1);
+            sum_store.releaseRef();
+            done();
+
+        })
+    });
+
+    //sum_store.
+
+    //
+    // acao.exec=(r){
+    //    return {x}
+    // }
+    // acao.exec(estoriaref);
+    // acao.on()
+
+    // var s = createStore(0, instance);
+    // expect(s.refCount(1), 's.refCount').to.equals(0);
+    // var ref1 = s.addRef(1);
+    // ref1.
+    //
+    // key = 1;
+    // ref1.data().name = "Anne";
+    // ref1.data().age = 20;
+    // expect(s.refCount(1), 's.refCount 1').to.equals(1);
+    // var ref2 = s.addRef(1);
+    // expect(s.refCount(1), 's.refCount 2').to.equals(2);
+    // expect(ref2.data.name, "ref2.data.name").to.equals("Anne");
+    // expect(ref2.data.age, "ref2.data.age").to.equals(20);
+    // ref2.data.age = 21;
+    // expect(ref1.data.age, "ref1.data.name").to.equals(21);
+    // ref1.releaseRef();
+    // expect(s.refCount(1), 's.refCount -1').to.equals(1);
+    // ref1.releaseRef();
+    // expect(s.refCount(1), 's.refCount -1r').to.equals(1);
+    // ref2.releaseRef();
+    // expect(s.refCount(1), 's.refCount -2').to.equals(0);
+    // ref2.releaseRef();
+    // expect(s.refCount(1), 's.refCount -2r').to.equals(0);
+    // done();
 
     it('add a todo', (done) => {
         //   var sample = TodoItemStore.addRef();
